2941e6e95f1fb43fdd9b5f471144321ea14352a3
C-Mike576 <m.calkins576@gmail.com>
2020-01-29 15:32:46 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,13 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
+    
   end
 
   def new
     @song = Song.new
+    @song.notes.build
+    @song.notes.build
   end
 
   def create
@@ -47,7 +50,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name,
+       :genre_id, notes_attributes: [:content])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  accepts_nested_attributes_for :notes
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+  end
+
+  def artist_name
+    self.artist.try(:name)
+  end
+
 end
